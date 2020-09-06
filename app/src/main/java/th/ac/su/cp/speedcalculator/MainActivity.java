01c13fc579ad713d46de4eda8fa2dd60e2efdc71
Nontaphat <zucktang@gmail.com>
2020-09-06 11:30:44 +0700

    07600468

--- app/src/main/java/th/ac/su/cp/speedcalculator/MainActivity.java
+++ app/src/main/java/th/ac/su/cp/speedcalculator/MainActivity.java
@@ -1,8 +1,15 @@
 package th.ac.su.cp.speedcalculator;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button calculate = findViewById(R.id.calculate);
+
+        Button button_clear = findViewById(R.id.clear);
+        button_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText Distance = findViewById(R.id.distance);
+                EditText Time = findViewById(R.id.time);
+                TextView Result = findViewById(R.id.result);
+                Distance.setText("");
+                Time.setText("");
+                Result.setText("");
+
+            }
+        });
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText distance = findViewById(R.id.distance);
+                EditText time = findViewById(R.id.time);
+                TextView Result = findViewById(R.id.result);
+                String Distance = distance.getText().toString();
+                String Time = time.getText().toString();
+                if(Distance.isEmpty()||Time.isEmpty()){
+                    Toast t = Toast.makeText(MainActivity.this, R.string.Distanceandtimearerequired,Toast.LENGTH_LONG);
+                    t.show();
+                }
+                else {
+                    Double S = Double.parseDouble(Distance);
+                    Double T = Double.parseDouble(Time);
+                    if(T==0){
+                        Toast t = Toast.makeText(MainActivity.this, R.string.Timemustbegreaterthanzero,Toast.LENGTH_LONG);
+                        t.show();
+                    }
+                    else {
+                        Double ANS = (S / 1000) * (3600 / T);
+                        Result.setText(String.format("%.2f", ANS));
+                        if(ANS>80){
+                            AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                            dialog.setTitle("SPEED CALCULATOR");
+                            dialog.setMessage(R.string.Speedisoverlimit);
+                            dialog.setPositiveButton("OK",null);
+                            dialog.show();}
+                    }
+                }
+
+
+            }
+        });
+
     }
 }
